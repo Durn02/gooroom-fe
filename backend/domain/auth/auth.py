5c1cff24926d6812d0d06b9c68741b1bee11107f
Durn02 <engus9902@gmail.com>
2024-06-23 23:29:37 +0900

    Chore: 패키기 import 수정

--- backend/domain/auth/auth.py
+++ backend/domain/auth/auth.py
@@ -1,1 +1,29 @@
 # backend/domain/auth/auth.py
+import os
+import sys
+from fastapi import HTTPException, APIRouter, Depends
+from config.connection import create_gremlin_client
+from utils.logger import Logger
+
+logger = Logger("domain.auth").get_logger()
+
+sys.path.append(
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+)
+
+router = APIRouter()
+
+
+# 노드 정보 조회(테스트용)
+@router.get("/")
+async def get_nodes(client=Depends(create_gremlin_client)):
+    logger.info("노드 정보 조회")
+    try:
+        query = "g.V()"
+        result = client.submit(query).all().result()
+        logger.info("/nodes 200 ok")
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        client.close()
--- backend/domain/service/content/content.py
+++ backend/domain/service/content/content.py
@@ -1,1 +1,4 @@
 # backend/domain/service/content/content.py
+from fastapi import APIRouter
+
+router = APIRouter()
