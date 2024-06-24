e6205ee8d50d60842ee42ec4ca5646715801603d
DuHyeonChoi <engus9902@gmail.com>
2024-06-25 02:21:07 +0900

    Chore: request객체 concern 자료형 json -> list 변경

--- backend/domain/auth/request/signup_request.py
+++ backend/domain/auth/request/signup_request.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel, EmailStr
 from typing import Dict
 
+
 class SignUpRequest(BaseModel):
     email: EmailStr
     password: str
-    concern: Dict[str, str]
+    concern: list
     nickname: str
     username: str
