3dcd7cca3c2828868d8f88eb636e046ef1a0ee9a
Dcaffein <hj02117@korea.ac.kr>
2024-07-14 17:49:26 +0900

    feat:friend/block/pop-members

--- backend/domain/service/friend/block/block.py
+++ backend/domain/service/friend/block/block.py
@@ -4,8 +4,8 @@ from fastapi import APIRouter,HTTPException, APIRouter, Depends, Body, Request
 from utils import verify_access_token
 from config.connection import create_gremlin_client
 from gremlin_python.process.traversal import T
-from .request import BlockFriendRequest
-from .response import BlockFriendResponse,GetBlockedResponse
+from .request import BlockFriendRequest,PopBlockedRequest
+from .response import BlockFriendResponse,GetBlockedResponse, PopBlockedResponse
 from typing import List
 
 router = APIRouter()
@@ -21,21 +21,23 @@ async def block_friend(
     user_node_id = verify_access_token(token)['user_node_id']
 
     try:
-        query = f"""g.V('{user_node_id}').as('from_user_node')
-        .V('{block_friend_request.user_node_id}').as('to_user_node')
+        query = f"""g.V('{user_node_id}')
+        .outE('block').where(inV().hasId('{block_friend_request.user_node_id}')).fold()
         .coalesce(
-        __.select('from_user_node').outE('block').where(inV().as('to_user_node')),
-        __.addE('block').from('from_user_node').to('to_user_node'))
+            unfold().constant('already block exist'),
+            addE('block').from(V('{user_node_id}')).to(V('{block_friend_request.user_node_id}'))
+        )
         """
 
         future_result_set = client.submitAsync(query).result().all()
         results = await asyncio.wrap_future(future_result_set)
 
-        if not len(results):
+        if results:
+            if results[0] == 'already knock exists':
+                raise HTTPException(status_code=400, detail="already knock exists")
+            return BlockFriendResponse()
+        else:
             raise HTTPException(status_code=404, detail=f'no such user {block_friend_request.user_node_id}')
-
-        print(results[0])
-        return BlockFriendResponse()
 
     except HTTPException as e :
         raise e
@@ -45,7 +47,7 @@ async def block_friend(
         client.close()
 
 @router.post("/get-members",response_model = List[GetBlockedResponse])
-async def get_members(
+async def get_blocked(
     request: Request,
     client = Depends(create_gremlin_client),
 ):
@@ -62,6 +64,31 @@ async def get_members(
         response =  [GetBlockedResponse.from_data(result['block_edge'], result['blocked_user']) for result in results]
         return response
 
+    except HTTPException as e :
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        client.close()
+
+@router.post("/pop-members")
+async def pop_blocked(
+    request: Request,
+    client = Depends(create_gremlin_client),
+    pop_blocked_request: PopBlockedRequest = Body(...),
+):
+    token = request.cookies.get(access_token)
+    verify_access_token(token)['user_node_id']
+
+    try:
+        query = f"""
+        g.E('{pop_blocked_request.block_edge_id}').drop()
+        """
+
+        future_result_set = client.submitAsync(query).result().all()
+        await asyncio.wrap_future(future_result_set)
+
+        return PopBlockedResponse()
     except HTTPException as e :
         raise e
     except Exception as e:
--- backend/domain/service/friend/block/request/block_friend_request.py
+++ backend/domain/service/friend/block/request/block_friend_request.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
 
 class BlockFriendRequest(BaseModel):
-    user_node_id:str = "blocked succesfully"
+    user_node_id:str
     
