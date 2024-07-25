8aaed2e0ef9f86a00f77c6be7e40d4ff29d47d41
unknown <ms020221@naver.com>
2024-07-25 20:17:52 +0900

    feat-admin-user-delelte

--- backend/domain/admin/admin.py
+++ backend/domain/admin/admin.py
@@ -1,5 +1,6 @@
 # backend/domain/admin/admin.py
-from fastapi import APIRouter, HTTPException, Depends, Request, Response
+import os, sys, json, asyncio, re
+from fastapi import HTTPException, APIRouter, Depends, Body, Request, Response
 from config.connection import create_gremlin_client
 from gremlin_python.driver.client import Client
 from gremlin_python.process.traversal import T
@@ -10,6 +11,9 @@ from utils import (
     verify_access_token,
     Logger,
 )
+from .response import DeleteUserResponse
+from .request import DeleteUserRequest
+
 logger = Logger(__file__)
 
 router = APIRouter()
@@ -22,38 +26,44 @@ async def delete_user(
     response: Response,
     request: Request,
     client=Depends(create_gremlin_client),
+    delete_user_request: DeleteUserRequest = Body(...),
 ):
     logger.info("admin delete user")
     try:
-        token = request.cookies.get("access_token")
+        token = request.cookies.get(access_token)
         print("token :", token)
         if not access_token:
             raise HTTPException(status_code=401, detail="Access token missing")
 
         token_payload = verify_access_token(token)
-        uuid = token_payload.get("uuid")
-        print(uuid)
+        user_node_id = token_payload.get("user_node_id")
 
-        if not uuid:
+        if not user_node_id:
             raise HTTPException(status_code=400, detail="Invalid input")
 
+        deleted_user_node_id = delete_user_request.deletedUserNodeId
         delete_user_query = f"""
-            g.V('{uuid}').fold().coalesce(
-                unfold().as('p').out('is_info').hasLabel('User').as('u')
-                .sideEffect(select('u').unfold().drop()).sideEffect(select('p').unfold().drop())
-                .constant('User deleted successfully'), constant('User not found')
-            )
+        g.V('{user_node_id}').has('grant', 'admin').fold().coalesce(
+            unfold().V('{deleted_user_node_id}').has('grant', 'member').fold().coalesce(
+                unfold().as('p').inE('is_info').outV().as('u')
+                .sideEffect(select('u').drop())
+                .sideEffect(select('p').drop())
+                .constant('User deleted successfully'),
+                constant('User not found')
+            ),
+            constant('Cannot delete this user')
+        )
         """
 
         future_result_set = client.submitAsync(delete_user_query).result().all()
         results = await asyncio.wrap_future(future_result_set)
-        print(results)
-        if results[0] == "User not found":
-            raise HTTPException(status_code=404, detail="User not found")
+
+        if not results:
+            raise HTTPException(status_code=404, detail="Not verified")
         if results[0] == "User deleted successfully":
-            response.delete_cookie(key="access_token")
-            return SignOutResponse()
+            response.delete_cookie(key=access_token)
+            return DeleteUserResponse()
         else:
-            raise HTTPException(status_code=500, detail="Failed to sign out")
+            raise HTTPException(status_code=500, detail="{results[0]}")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
