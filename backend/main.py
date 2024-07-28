a486a9c5a72e300855e03c633c34e11c0c085d04
DuHyeonChoi <engus9902@gmail.com>
2024-07-28 23:06:26 +0900

    Chore: main.py 린트 적용 및 사용안하는 import 제거

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,17 @@
-from fastapi import FastAPI,Depends
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from domain.api import router as domain_api_router
 from utils import Logger
 from config.connection import create_ssh_tunnel
-from fastapi.middleware.cors import CORSMiddleware
-from contextlib import asynccontextmanager
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from domain.service.content.content import delete_old_stickers,delete_old_casts
 from domain.service.friend.friend import delete_knock_links
+from domain.service.content.content import delete_old_stickers, delete_old_casts
 
 scheduler = AsyncIOScheduler()
 logger = Logger("main.py")
 tunnel = None
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -20,14 +21,15 @@ async def lifespan(app: FastAPI):
     logger.info("SSH 터널이 시작되었습니다. Neptune 데이터베이스에 연결합니다...")
     scheduler.start()
     logger.info("스케줄러가 실행되었습니다.")
-    scheduler.add_job(func=delete_old_stickers, trigger='cron',hour=0,minute=0)
-    scheduler.add_job(func=delete_old_casts, trigger='cron', minute='*/30')
-    scheduler.add_job(func=delete_knock_links, trigger='cron', hour='*/1')
+    scheduler.add_job(func=delete_old_stickers, trigger="cron", hour=0, minute=0)
+    scheduler.add_job(func=delete_old_casts, trigger="cron", minute="*/30")
+    scheduler.add_job(func=delete_knock_links, trigger="cron", hour="*/1")
     yield
     tunnel.stop()
     logger.info("SSH 터널이 종료되었습니다.")
     scheduler.shutdown()
     logger.info("스케줄러가 종료되었습니다.")
+
 
 app = FastAPI(lifespan=lifespan)
 
