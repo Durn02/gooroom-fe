ef1a778a9383737a9526ef18eb660f46fda2e55e
kongms002 <ms020221@naver.com>
2024-08-13 17:12:48 +0900

    fix:verification-code sent

--- backend/domain/auth/auth.py
+++ backend/domain/auth/auth.py
@@ -45,60 +45,47 @@ access_token = "access_token"
 @router.post("/send-verification-code")
 async def send_verification_code(
     response: Response,
-    driver=Depends(get_session),
+    session=Depends(get_session),
     send_verification_code_request: SendVerificationCodeRequest = Body(...),
 ):
     logger.info("send verify code")
 
     try:
-        # Verification count 조회
-        query = """
-        MATCH (p:PrivateData {email: $email})
-        RETURN p.verification_count AS verification_count
-        """
-        session = driver.session()
-        result = session.run(query, email=send_verification_code_request.email)
-        record = result.single()
-
-        if not record:
-            raise HTTPException(status_code=400, detail="User not found")
-
-        verification_count = record["verification_count"]
-
-        if verification_count >= 5:
-            raise HTTPException(
-                status_code=400,
-                detail="Exceeded the number of verification code requests",
-            )
-
-        # 새로운 verification count 및 정보 생성
-        new_verification_count = verification_count + 1
+        # Verification count 및 정보 업데이트 쿼리
         verification_code = "".join(
             random.choices(string.ascii_uppercase + string.digits, k=6)
         )
         expiration_time = datetime.now() + timedelta(minutes=30)
         verification_info = json.dumps({verification_code: expiration_time.isoformat()})
 
-        # 이메일로 verification code 전송
-        send_verification_email(send_verification_code_request.email, verification_info)
-
-        # Verification 정보 업데이트
         update_query = """
         MATCH (p:PrivateData {email: $email})
-        SET p.verification_count = $new_verification_count, 
+        WITH p
+        WHERE p.verification_count < 5
+        SET p.verification_count = p.verification_count + 1, 
             p.verification_info = $verification_info
         RETURN 'verification code sent' AS message
         """
+
         result = session.run(
             update_query,
             email=send_verification_code_request.email,
-            new_verification_count=new_verification_count,
             verification_info=verification_info,
         )
         update_record = result.single()
+        print(update_record)
+
+        if not update_record:
+            raise HTTPException(
+                status_code=400,
+                detail="User not found or exceeded verification attempts",
+            )
 
         if update_record["message"] != "verification code sent":
             raise HTTPException(status_code=400, detail="Error occurred")
+
+        # 이메일로 verification code 전송
+        send_verification_email(send_verification_code_request.email, verification_info)
 
         return SendVerificationCodeResponse(message="verification code sent")
 
@@ -111,7 +98,7 @@ async def send_verification_code(
 @router.post("/verify-code")
 async def verify_code(
     response: Response,
-    driver=Depends(get_session),
+    session=Depends(get_session),
     verification_request: VerificationRequest = Body(...),
 ):
     logger.info("verify code")
@@ -122,7 +109,6 @@ async def verify_code(
         MATCH (p:PrivateData {email: $email})
         RETURN p.verification_info AS verification_info, p.grant AS grant
         """
-        session = driver.session()
         result = session.run(query, email=verification_request.email)
         record = result.single()
 
@@ -168,7 +154,7 @@ async def verify_code(
 @router.post("/signup")
 async def signup(
     response: Response,
-    driver=Depends(get_session),
+    session=Depends(get_session),
     signup_request: SignUpRequest = Body(...),
 ):
     logger.info("signup")
@@ -186,26 +172,17 @@ async def signup(
         concern_json = json.dumps(signup_request.concern)
 
         query = """
-        MERGE (p:PrivateData {email: $email})
-        ON CREATE SET p.password = $password, 
-                      p.username = $username,
-                      p.link_info = '',
-                      p.verification_info = '',
-                      p.link_count = '0',
-                      p.verification_count = '0',
-                      p.grant = 'not-verified'
-        
-        MERGE (u:User {username: $username})
-        ON CREATE SET u.nickname = $nickname, 
-                      u.concern = $concern, 
-                      u.my_memo = ''
-        
+        MATCH (t:PrivateData {email: $email})
+        WITH t
+        WHERE t IS NULL
+        CREATE (p:PrivateData {email: $email, password: $password, username: $username, 
+                               link_info: "", verification_info: "", link_count: 0, 
+                               verification_count: 0, grant: "not-verified"})
+        CREATE (u:User {username: $username, nickname: $nickname, concern: $concern, my_memo: ""})
         MERGE (p)-[:is_info]->(u)
-
         RETURN p, u
         """
 
-        session = driver.session()
         result = session.run(
             query,
             email=signup_request.email,
@@ -237,7 +214,7 @@ async def signup(
 @router.post("/signin")
 async def signin(
     response: Response,
-    driver=Depends(get_session),
+    session=Depends(get_session),
     signin_request: SignInRequest = Body(...),
 ):
     logger.info("login")
@@ -245,11 +222,10 @@ async def signin(
     query = """
     MATCH (p:PrivateData {email: $email})
     MATCH (p)-[:is_info]->(u)
-    RETURN p.password AS password, u.grant AS grant, ID(u) AS id
+    RETURN p.password AS password, p.grant AS grant, ID(u) AS id
     """
 
     try:
-        session = driver.session()
         result = session.run(query, email=signin_request.email)
         record = result.single()
 
@@ -258,7 +234,7 @@ async def signin(
 
         password = record["password"]
         grant = record["grant"]
-
+        print(password, grant, record["id"])
         if not verify_password(signin_request.password, password):
             raise HTTPException(status_code=400, detail="inconsistent password")
 
@@ -294,7 +270,7 @@ async def logout(request: Request, response: Response):
 async def pw_change(
     request: Request,
     response: Response,
-    driver=Depends(get_session),
+    session=Depends(get_session),
     pw_change_req: PwChangeRequest = Body(...),
 ):
     token = request.cookies.get(access_token)
@@ -315,7 +291,6 @@ async def pw_change(
         WHERE ID(u) = $user_node_id
         RETURN p.password AS password
         """
-        session = driver.session()
         result = session.run(query, user_node_id=int(user_node_id))
         record = result.single()
 
@@ -345,7 +320,6 @@ async def pw_change(
         if not verify_password(pw_change_req.currentpw, password):
             raise HTTPException(status_code=400, detail="Incorrect current password")
 
-        # 비밀번호 업데이트
         update_query = """
         MATCH (u:User)<-[:is_info]-(p:PrivateData)
         WHERE ID(u) = $user_node_id
@@ -373,7 +347,7 @@ async def pw_change(
 async def signout(
     response: Response,
     request: Request,
-    driver=Depends(get_session),
+    session=Depends(get_session),
 ):
     logger.info("signout")
     try:
@@ -395,7 +369,6 @@ async def signout(
             RETURN 'User deleted successfully' AS message
         """
 
-        session = driver.session()
         result = session.run(delete_user_query, user_node_id=int(user_node_id))
         record = result.single()
 
