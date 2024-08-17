684459a9af90aa79ed027cd2e05e46caf1450e78
Durn02 <engus9902@gmail.com>
2024-08-18 01:31:42 +0900

    Chore: logger 적용

--- backend/utils/jwt_utils.py
+++ backend/utils/jwt_utils.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 from fastapi import HTTPException
 import jwt
+from .logger import Logger
 
 env_path = Path(__file__).resolve().parent.parent / ".env"
 load_dotenv(dotenv_path=env_path)
@@ -11,8 +12,11 @@ load_dotenv(dotenv_path=env_path)
 SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 ALGORITHM = "HS256"
 
+logger = Logger(__file__)
+
 
 def create_access_token(user_node_id: str) -> str:
+    logger.info("create access token(func)")
     to_encode = {
         "user_node_id": user_node_id,
         "exp": int((datetime.now() + timedelta(hours=1)).timestamp()),
@@ -22,6 +26,7 @@ def create_access_token(user_node_id: str) -> str:
 
 
 def create_refresh_token(user_node_id: str) -> str:
+    logger.info("create refresh token(func)")
     to_encode = {
         "user_node_id": user_node_id,
         "exp": int((datetime.now() + timedelta(days=30)).timestamp()),
@@ -43,6 +48,7 @@ def create_refresh_token(user_node_id: str) -> str:
 
 def verify_access_token(token: str) -> dict:
     try:
+        logger.info("verify access token(func)")
         payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
         if payload.get("exp") < int(datetime.now().timestamp()):
             raise HTTPException(status_code=401, detail="token has expired")
@@ -77,6 +83,7 @@ def verify_access_token(token: str) -> dict:
 
 def verify_refresh_token(token: str) -> dict:
     try:
+        logger.info("verify refresh token(func)")
         payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
         if payload.get("exp") < int(datetime.now().timestamp()):
             raise HTTPException(status_code=401, detail="token has expired")
