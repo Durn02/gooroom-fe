602859b1e7e90f0202d43a72180a8eaa252262c4
Dcaffein <hj02117@korea.ac.kr>
2024-09-03 13:24:16 +0900

    fix:signup_request nickname min_length=1

--- backend/domain/auth/request/signup_request.py
+++ backend/domain/auth/request/signup_request.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr,Field
 from typing import List
 
 class SignUpRequest(BaseModel):
     email: EmailStr
     password: str
     concern: List[str]
-    nickname: str
+    nickname: str = Field(..., min_length=1)
     username: str
