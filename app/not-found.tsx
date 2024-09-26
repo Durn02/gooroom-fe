b006c9bef66c3b5d07c976f803cdc28b63007252
Minseok Choe <choems@outlook.com>
2024-09-27 06:03:21 +0900

    Move file

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,13 @@
-import NoPage from "@/lib/pages/nopage/NoPage";
+import DefaultButton from "@/components/Button/DefaultButton";
+import Link from "next/link";
 
-export default NoPage
+export default function NoPage() {
+  return (
+    <div>
+      <h1>404 not found</h1>
+      <Link href={"/"}>
+        <DefaultButton placeholder="go to landing page" />
+      </Link>
+    </div>
+  );
+}
