653e2b49e83cbfb49f5f8d92770fe684f5276d7d
Dcaffein <hj02117@korea.ac.kr>
2024-10-02 17:23:01 +0900

    fix:alertCasting vis-network DataSet -> vis-data DataSet

--- lib/utils/alertCasting.ts
+++ lib/utils/alertCasting.ts
@@ -1,7 +1,7 @@
-import { DataSet } from "vis-data";
-import { Node } from "vis-network";
-import getAPIURL from "./getAPIURL";
-import { GetCastsResponse } from "../types/landingPage.type";
+import { DataSet } from 'vis-data';
+import { Node } from 'vis-network';
+import { GetCastsResponse } from '../types/landingPage.type';
+import { API_URL } from '@/lib/utils/config';
 
 const unreadCasts: GetCastsResponse[] = [];
 
@@ -9,17 +9,14 @@ const alertFrameInterval = 100;
 const alertDuration = 5600;
 const alertInterval = 10000;
 
-export const fetchUnreadCasts = async (nodesDataset: DataSet<Node>) => {
-  const APIURL = getAPIURL();
+export const fetchUnreadCasts = async (nodesDataset: DataSet) => {
+  // const APIURL = getAPIURL();
   try {
-    const response = await fetch(
-      `${APIURL}/domain/content/cast/get-unread-members`,
-      {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-        credentials: "include",
-      }
-    );
+    const response = await fetch(`${API_URL}/domain/content/cast/get-unread-members`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+    });
     if (response.ok) {
       const data = await response.json();
       if (data.contents.length > 0) {
@@ -31,30 +28,27 @@ export const fetchUnreadCasts = async (nodesDataset: DataSet<Node>) => {
       long_poll(nodesDataset);
     }, alertInterval);
   } catch (error) {
-    console.error("error: ", error);
+    console.error('error: ', error);
     setTimeout(() => {
       fetchUnreadCasts(nodesDataset);
     }, alertInterval);
   }
 };
 
-const long_poll = async (nodesDataset: DataSet<Node>) => {
-  console.log("long_poll called");
+const long_poll = async (nodesDataset: DataSet) => {
+  console.log('long_poll called');
 
-  const APIURL = getAPIURL();
+  // const APIURL = getAPIURL();
   try {
-    const response = await fetch(
-      `${APIURL}/domain/content/cast/get-unsent-members`,
-      {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-        credentials: "include",
-      }
-    );
+    const response = await fetch(`${API_URL}/domain/content/cast/get-unsent-members`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+    });
 
     if (response.ok) {
       const data = await response.json();
-      console.log("long_poll data : ", data);
+      console.log('long_poll data : ', data);
       if (data.contents.length > 0) {
         unreadCasts.push(...data.contents);
 
@@ -65,16 +59,16 @@ const long_poll = async (nodesDataset: DataSet<Node>) => {
       setTimeout(() => long_poll(nodesDataset), alertInterval);
     }
   } catch (error) {
-    console.error("Error during long polling: ", error);
+    console.error('Error during long polling: ', error);
     setTimeout(() => long_poll(nodesDataset), alertInterval);
   }
 };
 
-const alertUnreadCast = (nodesDataset: DataSet<Node>) => {
+const alertUnreadCast = (nodesDataset: DataSet) => {
   unreadCasts.forEach(async (cast) => {
     const node_id = cast.creator.node_id;
     const originalNodeProps = nodesDataset.get(node_id);
-    const originalSize = originalNodeProps?.group == "neighbor" ? 10 : 15;
+    const originalSize = originalNodeProps?.group == 'neighbor' ? 10 : 15;
 
     let growing = true;
     let newOpacity = 1;
