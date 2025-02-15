4e8ce24281d0841822a1f82fa78f3b5f7ed1c9ac
Durn02 <engus9902@gmail.com>
2025-02-16 05:22:52 +0800

    Feat: accept and reject knock button in knock list modal

--- src/components/Modals/KnockListModal/KnockListModal.tsx
+++ src/components/Modals/KnockListModal/KnockListModal.tsx
@@ -1,6 +1,8 @@
 // KnockListModal.tsx
 'use client';
+import { acceptKnock, rejectKnock } from '@/src/lib/api/knock.api';
 import { KnockEdge } from '@/src/types/landingPage.type';
+import { clearStore } from '@/src/utils/indexedDB';
 import React, { useEffect, useState, useCallback } from 'react';
 
 interface KnockListModalProps {
@@ -44,6 +46,34 @@ const KnockListModal: React.FC<KnockListModalProps> = ({ isOpen, onClose, knocks
     }
   };
 
+  const accept_knock = async (knockId: string) => {
+    const select = window.confirm('노크를 수락하시겠습니까?');
+    if (!select) return;
+    const data = await acceptKnock(knockId);
+    Promise.all([clearStore('roommates'), clearStore('neighbors')]);
+    window.location.reload();
+    if (!data) {
+      console.error('Failed to accept knock');
+      alert('노크 수락을 실패했습니다');
+    } else {
+      alert('노크를 수락했습니다');
+    }
+    onClose();
+  };
+
+  const reject_knock = async (knockId: string) => {
+    const select = window.confirm('노크를 거절하시겠습니까?');
+    if (!select) return;
+    const data = await rejectKnock(knockId);
+    if (!data) {
+      console.error('Failed to reject knock');
+      alert('노크 거절을 실패했습니다');
+    } else {
+      alert('노크를 거절했습니다');
+    }
+    onClose();
+  };
+
   if (!isVisible) return null;
 
   return (
@@ -66,11 +96,38 @@ const KnockListModal: React.FC<KnockListModalProps> = ({ isOpen, onClose, knocks
           </svg>
         </button>
         <h2 className="text-2xl font-bold mb-4">Knock List</h2>
-        {knocks && knocks.length > 0 ? ( // Add null check here
+        {knocks && knocks.length > 0 ? (
           <ul className="space-y-4">
             {knocks.map((knock, i) => (
-              <li key={i} className="border-b pb-2">
+              <li key={i} className="border-b pb-2 flex items-center justify-between hover:bg-gray-100">
                 <p className="text-lg mb-1">{knock.nickname}</p>
+                <div>
+                  <button
+                    onClick={() => accept_knock(knock.edge_id)}
+                    className="text-green-500 hover:text-green-700 mr-2"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
+                    </svg>
+                  </button>
+                  <button onClick={() => reject_knock(knock.edge_id)} className="text-red-500 hover:text-red-700">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </button>
+                </div>
               </li>
             ))}
           </ul>
