e7f6b31337be9f7641b63e22871cebf07773933d
Durn02 <engus9902@gmail.com>
2025-05-16 16:07:45 +0900

    Fix: config.ts수정

--- app/src/lib/config.ts
+++ app/src/lib/config.ts
@@ -1,6 +1,6 @@
-export const IS_DEV = process.env.NODE_ENV === 'development';
-export const IS_PROD = process.env.NODE_ENV === 'production';
+export const IS_DEV = process.env.NEXT_PUBLIC_NODE_ENV === 'development';
+export const IS_PROD = process.env.NEXT_PUBLIC_NODE_ENV === 'production';
 
 export const API_URL_DEV = 'http://localhost:8000';
-export const API_URL_PROD = process.env.BACKEND_URL;
+export const API_URL_PROD = process.env.NEXT_PUBLIC_BACKEND_URL;
 export const API_URL = IS_DEV ? API_URL_DEV : API_URL_PROD;
