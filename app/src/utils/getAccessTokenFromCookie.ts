f4ea28ed601d9f7b12cd5051fa3da49fb6ee741f
Dcaffein <hj02117@korea.ac.kr>
2025-05-16 15:10:12 +0900

    delete unnecessary file

--- app/src/utils/getAccessTokenFromCookie.ts
+++
@@ -1,8 +0,0 @@
-'use client';
-
-// 쿠키에서 access_token을 가져오는 함수
-export function getAccessTokenFromCookie(cookie_name: string): string {
-  const cookie = globalThis?.document.cookie.split('; ').find((row) => row.startsWith(`${cookie_name}=`));
-
-  return cookie ? decodeURIComponent(cookie.split('=')[0]) : '';
-}
