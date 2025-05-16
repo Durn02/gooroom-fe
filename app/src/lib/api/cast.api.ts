58411aabf816be6b2bb894008f838f3749279faf
Dcaffein <hj02117@korea.ac.kr>
2025-05-16 15:36:48 +0900

    fix: add cast.api.ts

---
+++ app/src/lib/api/cast.api.ts
@@ -0,0 +1,23 @@
+'use client';
+
+import apiClient from '../api/axiosApiClient';
+
+interface CastProps {
+  message: string;
+  friends: string[];
+  duration: number;
+}
+
+export const createCast = async ({ message, friends, duration }: CastProps) => {
+  try {
+    const response = await apiClient.post('/domain/content/cast/create', {
+      friends: friends,
+      message: message,
+      duration: duration,
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Failed to create cast:', error);
+    throw error;
+  }
+};
--- app/src/lib/sign.ts
+++ app/src/lib/sign.ts
@@ -1,7 +1,6 @@
 import { clearStore } from '../utils/indexedDB';
 
 export const logout = () => {
-  // window.location.href = '/signin';
   window.location.href = '/main';
   clearStore('roommates');
   clearStore('neighbors');
